26cf064a822ad8318a9016e17e1c6367aeb4bf74
s0594520 <Ahmadbaz.Chedo@Student.HTW-Berlin.de>
2025-05-26 00:48:52 +0200

    test 3

--- src/main/java/de/htwberlin/webtech/entity/Book.java
+++ src/main/java/de/htwberlin/webtech/entity/Book.java
@@ -4,22 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Book {
+     private Long id;
     private String title;
     private String author;
     private List<Integer> chapters = new ArrayList<>();
     private String summary;
     private String genre;
     private String readingProgress;
-
-    public Book(String title, String author, String summary, String genre, String readingProgress) {
+public Book(Long id, String title, String author, String summary, String genre, String readingProgress) {
+        this.id = id;
         this.title = title;
         this.author = author;
         this.summary = summary;
         this.genre = genre;
         this.readingProgress = readingProgress;
+    
     }
 
     public Book() { }
+
+     public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public void setTitle(String title) {
         this.title = title;
--- src/main/java/de/htwberlin/webtech/repository/bookRepository.java
+++ src/main/java/de/htwberlin/webtech/repository/bookRepository.java
@@ -17,21 +17,20 @@ public class bookRepository {
     }
 
     @PostConstruct
-    private void init() {
-        Book theAlchemist = new Book(
-                "The Alchemist",
-                "Paulo Coelho",
-                "An Andalusian shepherd boy named Santiago travels from Spain to the Egyptian pyramids in search of treasure. "
-                        + "He learns to follow his dreams, listen to his heart, and read the omens.",
-                "Fiction",
-                "Finished"
-        );
+  private void init() {
+    Book theAlchemist = new Book(
+            1L, // ID gesetzt!
+            "The Alchemist",
+            "Paulo Coelho",
+            "An Andalusian shepherd boy named Santiago travels from Spain to the Egyptian pyramids in search of treasure.",
+            "Fiction",
+            "Finished"
+    );
 
-        // Beispielkapitel hinzufügen
-        theAlchemist.addChapter(1);
-        theAlchemist.addChapter(2);
-        theAlchemist.addChapter(3);
+    theAlchemist.addChapter(1);
+    theAlchemist.addChapter(2);
+    theAlchemist.addChapter(3);
 
-        books.add(theAlchemist);
+    books.add(theAlchemist);
     }
-}
+    }
