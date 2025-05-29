34b4961d5d7287e41c171d790f0edefb7586ad23
Eric Goschlich <Eric.Gotschlich@Student.HTW-Berlin.de>
2025-05-29 19:25:31 +0200

    h

--- src/main/java/de/htwberlin/webtech/controller/BookController.java
+++ src/main/java/de/htwberlin/webtech/controller/BookController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/movie")
+@RequestMapping("/books")
 @CrossOrigin(origins = {"http://localhost:5173", "https://booknestfrontend-iaq4.onrender.com"})
 
 public class BookController {
