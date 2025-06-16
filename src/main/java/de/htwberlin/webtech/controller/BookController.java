b38deadd37b4a46012ed287e59443fe9ca119ae6
s0594520 <Ahmadbaz.Chedo@Student.HTW-Berlin.de>
2025-06-16 02:25:32 +0200

    application fix

--- src/main/java/de/htwberlin/webtech/controller/BookController.java
+++ src/main/java/de/htwberlin/webtech/controller/BookController.java
@@ -1,7 +1,12 @@
 package de.htwberlin.webtech.controller;
 
 import de.htwberlin.webtech.entity.Book;
-import de.htwberlin.webtech.repository.bookRepository;
+import de.htwberlin.webtech.service.BookService;
+import jakarta.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,14 +16,20 @@ import java.util.List;
 @RequestMapping("/books")
 public class BookController {
 
-    private final bookRepository repository;
+    private final BookService bookService;
 
-    public BookController(bookRepository repository) {
-        this.repository = repository;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
     }
 
-    @GetMapping
-    public List<Book> getAllBooks() {
-        return repository.findAll();
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Iterable<Book>> getAllBooks() {
+        final Iterable<Book> result = bookService.getAllBooks();
+        return ResponseEntity.ok(result);
+    }
+    @PostMapping
+    public ResponseEntity<Book> addBook(@Valid @RequestBody final Book book) {
+        final Book created = bookService.addBook(book);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
 }
