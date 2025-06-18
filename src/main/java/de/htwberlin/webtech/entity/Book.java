e2f3ba38d56127092979c9f986850b776516bb23
s0594520 <Ahmadbaz.Chedo@Student.HTW-Berlin.de>
2025-06-18 04:05:13 +0200

    identity fix

--- src/main/java/de/htwberlin/webtech/entity/Book.java
+++ src/main/java/de/htwberlin/webtech/entity/Book.java
@@ -1,13 +1,23 @@
 package de.htwberlin.webtech.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Book {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private Long id;
 
     private String title;
@@ -20,68 +30,6 @@ public class Book {
     private String genre;
     private String readingProgress;
 
-    public Book(Long id, String title, String author, String summary, String genre, String readingProgress) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.summary = summary;
-        this.genre = genre;
-        this.readingProgress = readingProgress;
-    }
 
-    public Book() { }
 
-     public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getTitle() {
-        return title;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setChapters(List<Integer> chapters) {
-        this.chapters = chapters;
-    }
-    public List<Integer> getChapters() {
-        return chapters;
-    }
-
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-    public String getSummary() {
-        return summary;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-    public String getGenre() {
-        return genre;
-    }
-
-    public void addChapter(int chapterNumber) {
-        chapters.add(chapterNumber);
-    }
-
-    public void setReadingProgress(String readingProgress) {
-        this.readingProgress = readingProgress;
-    }
-    public String getReadingProgress() {
-        return readingProgress;
-    }
 }
